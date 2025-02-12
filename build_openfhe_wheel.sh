48d7d6f8c0d82bd487009a2003847aca614a47ce
Dmitriy Suponitskiy <dsuponitskiy@dualitytech.com>
2025-02-12 11:22:43 -0500

    Added get-env.sh and the option WITH_STATIC_OPENFHE_LIBS to the openfhe-python build

---
+++ build_openfhe_wheel.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+. ./scripts/common-functions.sh
+
+# find the directory where the script is and where we will run this script from
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+# echo "Script absolute path: $SCRIPT_DIR"
+cd $SCRIPT_DIR
+
+# create and/or activate the virtual environment for the build
+. ./scripts/get-env.sh
+separator
+echo "Activated virtual environment: \"$(basename "$VIRTUAL_ENV")\""
+separator
+
+# build openfhe-development and openfhe-python
+./scripts/build-binaries.sh
+
+# build the wheel
+./scripts/compose-openfhe-python-wheel.sh
---
+++ scripts/get-env.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# create and/or activate the virtual environment for the build
+VENV_DIR="./env_for_openfhe_wheel"
+if [ ! -d ${VENV_DIR} ]; then
+    python3 -m venv ${VENV_DIR}
+    . ${VENV_DIR}/bin/activate
+
+    # install all necessary packages
+    pip install "pybind11[global]"
+    # pip install pybind11-stubgen
+    pip install --upgrade pip setuptools wheel build
+else
+    # source ${VENV_DIR}/bin/activate
+    . ${VENV_DIR}/bin/activate
+fi
+
